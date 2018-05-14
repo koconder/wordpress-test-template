e1ed030d24244c6263cc67ce52a88a0a07a3ca69
Vincent K <koconder@gmail.com>
2018-05-14 21:35:17 +1000

    Merging changes from dev-branch

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name: Wordpress Test Template Plugin
+ * Version: 1.0.0
+ * Description: Dummy plugin to pass wordpress testing
+ * Author: Vincent Koc
+ * Author URI: https://github.com/koconder/
+ * Plugin URI: https://github.com/koconder/wordpress-test-template/
+ * Text Domain: wordpresstesttanplate
+ * Domain Path: /languages
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0
+*/
+function wordpresstesttemplate(){
+	return true;
+}
+//dummy plugin test
+wordpresstesttemplate();
