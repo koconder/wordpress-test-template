b06045a4b8869ab8fa375271a6c6b2cdbeb8e8c8
Vincent K <koconder@gmail.com>
2019-01-21 15:29:21 +1100

    Test fix for PHPCS

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -225,9 +225,9 @@ start_server() {
 
 run_phpcs() {
 	pear config-set auto_discover 1
-	pear install PHP_CodeSniffer
+	#pear install PHP_CodeSniffer
 	git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
-	phpenv rehash
+	#phpenv rehash
 	npm install -g jshint
 	phpcs --config-set installed_paths $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
 	phpcs -i
