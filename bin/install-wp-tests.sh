752ba50ed233b4b853913d898f58b3a1fe539a3d
Vincent K <koconder@gmail.com>
2019-01-21 16:50:26 +1100

    Typo

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -180,9 +180,9 @@ link_this_project() {
 	echo "WP-CLI Installing and Activating core $FOLDER_NAME ($WP_PROJECT_TYPE) for testing"
   # If we are running on the base code with no plugin, use the dummy plugin which is bundled
   if [ $FOLDER_NAME = "wordpress-test-template" ]; then
-    cp -rf $FOLDER_PATH/test-plugin/boilerplate-dummy-plugin/ {$WP_CORE_DIR}wp-content/plugins/boilerplate-dummy-plugin/
+    cp -rf $FOLDER_PATH/test-plugin/boilerplate-dummy-plugin/ $WP_CORE_DIR/wp-content/plugins/boilerplate-dummy-plugin/
   else
-    cp -rf $FOLDER_PATH {$WP_CORE_DIR}wp-content/plugins/
+    cp -rf $FOLDER_PATH $WP_CORE_DIR/wp-content/plugins/
   fi
 
 	case $WP_PROJECT_TYPE in
